aff10a733027ce496fd4cbb32bdff89bb8b87446
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-07-14 21:09:53 +0600

    icon updated

--- src/components/All components/Navbar.jsx
+++ src/components/All components/Navbar.jsx
@@ -5,17 +5,16 @@ export default function Navbar() {
     <>
     <div className="h-[100px]">
       <Sidebar></Sidebar>
-      <div className="w-3/4 mx-auto flex justify-between items-center h-full">
+      <div className="w-10/12 mx-auto flex justify-between items-center h-full">
       <motion.span
          initial={{opacity:0, scale:0.5}}
          animate={{opacity:1, scale:1}}
          transition={{duration:0.5}}
         >Md.Akash Ahmed</motion.span>
        <div className="flex justify-center items-center gap-10">
-        <a href=""><img className="w-[20px] h-[20px] rounded-md" src='../../../public/facebook.png' alt="" /></a>
-        <a href=""><img className="w-[20px] h-[20px] rounded-md" src="../../../public/instagram.webp" alt="" /></a>
-        <a href=""><img className="w-[20px] h-[20px] rounded-md" src="../../../public/twitter.webp" alt="" /></a>
-        <a href=""><img className="w-[20px] h-[20px] rounded-md" src="../../../public/youtube.jpg" alt="" /></a>
+        <a href="https://www.facebook.com/akashahmed.joy.775/" target="_blank"><img className="w-[20px] h-[20px] rounded-full" src='../../../public/fb.png' alt="" /></a>
+        <a href="https://www.instagram.com/akashjoy9/" target="_blank"><img className="w-[20px] h-[20px] rounded-full" src="../../../public/insta.jpg" alt="" /></a>
+        <a href="https://github.com/AkashAhmed007" target="_blank"><img className="w-[20px] h-[20px] rounded-full" src="../../../public/github.jfif" alt="" /></a>
         </div>
       </div>
     </div>
